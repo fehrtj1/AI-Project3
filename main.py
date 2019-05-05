99c5c8725eb4d27407058535bf6fe7da09e0163c
Tyler Fehr <fehrtj1@gcc.edu>
2019-05-05 19:38:57 -0400

    More, deck creation works

--- main.py
+++ main.py
@@ -10,16 +10,18 @@ class Game:
         self.time_tokens = 8
         self.fuse_tokens = 3
         self.game_lost = False
-        self.clues = clues
+        self.clues = []
 
     def turn(self, player, action_number):
-        if action_number is 0: # Give one piece of information
+
+        if action_number is 0:  # Give one piece of information
             self.time_tokens -= 1
 
-        elif action_number is 1: # Discard a card
+        elif action_number is 1:  # Discard a card
             self.time_tokens += 1
-            player.draw()
-        elif action_number is 2: # Play a card
+            self.discard(self, player, )
+
+        elif action_number is 2:  # Play a card
 
 
             self.play(self, player, player.get_optimal_card())
@@ -48,22 +50,19 @@ class Game:
             print("card not in player's hand")
 
 
-
-
-
 class Card:
     def __init__(self, color, value):
         self.color = color
         self.value = value
 
 
-
-class Clues:
+class Clue:
     def __init__(self, colors, values, relevant_player):
         pass
 
 
 class Player:
+
     def __init__(self, hand):
         self.hand = hand
 
@@ -71,11 +70,55 @@ class Player:
         if card in self.hand:
             self.hand.remove(card)
             return
-        print("Card not in hand")
+        print("ERR: Card not in hand")
 
     def draw(self, deck):
         new_card = deck.pop()
         self.hand.append(new_card)
+        if len(deck) is 0:
+            print("One turn remaining, draw pile empty")
 
     def get_optimal_card(self):
         pass
+        # do something to judge what card should be played.
+
+def create_deck():
+    deck_length = 50
+    deck = []
+    colors = ['green', 'blue', 'yellow', 'red', 'white']
+
+    for color in colors:
+        for i in range(10):
+
+            if i is 0 or i is 1 or i is 2:
+                deck.append(Card(color, 1))
+            elif i is 3 or i is 4:
+                deck.append(Card(color, 2))
+            elif i is 5 or i is 6:
+                deck.append(Card(color, 3))
+            elif i is 7 or i is 8:
+                deck.append(Card(color, 4))
+            else:
+                deck.append(Card(color, 5))
+
+    if len(deck) is deck_length:
+        print("*Deck created successfully*")
+
+    return deck
+
+
+
+######### Game Loop
+
+deck = create_deck()
+for card in deck:
+    print(str(card.color) + " " + str(card.value))
+#hanabi = Game
+
+
+
+
+#while not
+
+
+
